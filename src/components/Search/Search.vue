2a30b2440f962c57a59bd878bcbcfb9bac21da72
Manuel Bravo <manuelbravo@gmail.com>
2020-04-23 22:55:08 +0200

    Implementing clear search input functionality

--- src/components/Search/Search.vue
+++ src/components/Search/Search.vue
@@ -9,6 +9,16 @@
         @keydown.enter="search"
         v-model.trim="searchTerm"
       >
+      <a
+        class="input-clear"
+        :class="{ hidden: !this.searchTerm }"
+        href="#"
+        title="Clear search"
+        @click.prevent="clearInput"
+        @keydown.space.enter="clearInput"
+      >
+        X
+      </a>
     </label>
     <button
       :aria-disabled="isLoading"
@@ -43,6 +53,9 @@ export default {
     ...mapActions({
       searchFor: 'getSearchResults',
     }),
+    clearInput() {
+      this.searchTerm = null;
+    },
     search() {
       // avoid empty search and repeat the last one
       if ((this.searchTerm && this.searchTerm !== '') && this.searchTerm !== this.lastSearchTerm) {
@@ -65,14 +78,26 @@ export default {
   border-radius: 4px;
   font-family: 'Montserrat', sans-serif;
   font-size: 1rem;
-  margin-right: 15px;
-  padding: 10px;
-  width: 200px;
+  padding: 10px 30px 10px 10px;
+  width: 175px;
 }
 
 @media (min-width: 768px) {
   #search-input {
-    width: 350px;
+    width: 325px;
   }
+}
+
+.input-clear {
+  color: black;
+  font-weight: bold;
+  margin: 0 15px 0 -25px;
+  opacity: 0.7;
+  padding: 10px 10px 10px 0;
+  text-decoration: none;
+}
+
+.hidden {
+  visibility: hidden;
 }
 </style>
